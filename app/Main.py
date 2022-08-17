484470d7c5c85e4f9ac8de4a3ae2a76f40f5380a
Michael J. Booth <mjboothaus@gmail.com>
2022-08-17 20:34:34 +1000

    Update Main.py

--- app/Main.py
+++ app/Main.py
@@ -1,1 +1,29 @@
 # Main.py -- for creating multipage app
+
+from pathlib import Path
+
+import streamlit as st
+
+# from helper_functions import read_render_markdown_file
+
+APP_TITLE = settings.APP_TITLE
+SUB_TITLE = settings.SUB_TITLE
+
+st.set_page_config(
+    page_title=APP_TITLE,
+    layout="wide",
+    menu_items={
+        "About":
+        "Created with love & care at DataBooth - www.databooth.com.au"
+    })
+
+
+def create_app_header(app_title, subtitle=None):
+    st.header(app_title)
+    if subtitle is not None:
+        st.subheader(subtitle)
+    return None
+
+create_app_header(APP_TITLE, SUB_TITLE)
+
+# read_render_markdown_file("docs/Main.md", output="streamlit")
