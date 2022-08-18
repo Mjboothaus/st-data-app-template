1e1a5c45e1ed8cfe5ce8689410c003f4d5429e58
Michael Booth <michael@databooth.com.au>
2022-08-18 15:25:59 +1000

    swapped dirtree.py for seedir package

--- app/pages/0_Utilities.py
+++ app/pages/0_Utilities.py
@@ -2,7 +2,9 @@ from pathlib import Path
 
 import streamlit as st
 from Main import APP_TITLE
-from src.dirtree import show_dir_tree
+#from src.dirtree import show_dir_tree
+
+from seedir import seedir
 
 
 TAB_NAMES = ["Directory structure", "Tab 2"]
@@ -11,12 +13,15 @@ TAB_NAMES = ["Directory structure", "Tab 2"]
 tab0, tab1 = st.tabs(TAB_NAMES)
 
 with tab0:
-  st.write(f"{APP_TITLE}: {TAB_NAMES[0]}")
-  
-  with st.spinner("Creating directory tree"):
-    tree = show_dir_tree(Path.cwd())
-    st.code(tree)
+    st.write(f"{APP_TITLE}: {TAB_NAMES[0]}")
 
-  
+    with st.spinner("Creating directory tree"):
+        #tree = show_dir_tree(Path.cwd())
+        # st.code(tree)
+        dir_str = seedir(path=Path.cwd().as_posix(), style='emoji',
+                         printout=False, itemlimit=10, depthlimit=2, exclude_folders='.git')
+        st.code(dir_str)
+
+
 with tab1:
-  st.write(f"{APP_TITLE}: {TAB_NAMES[1]}")
+    st.write(f"{APP_TITLE}: {TAB_NAMES[1]}")
